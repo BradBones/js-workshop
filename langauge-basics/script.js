c4ac7ab9dcb473576cdd0d2ad54d0c30f215d2af
Niza <niza@Nizas-MacBook-Pro-2.local>
2019-03-23 11:58:32 -0700

    gen update

--- langauge-basics/script.js
+++ langauge-basics/script.js
@@ -109,7 +109,7 @@
 
 
 /*********************
- * Challenge 1
+ * CODING CHALLENGE 1
  */
 
 // var markHeight = 1.7;
@@ -195,30 +195,113 @@
  * The Ternary Operator and Switch Statments
  */
 
-var firstName = 'John';
-var age = 16;
+// var firstName = 'John';
+// var age = 16;
 
-// Ternary opperator
-age >= 18 ? console.log(firstName + ' drinks beer.')
-: console.log(firstName + ' drinks juice.');
+// // Ternary opperator
+// age >= 18 ? console.log(firstName + ' drinks beer.')
+// : console.log(firstName + ' drinks juice.');
 
-var drink = age >= 18 ? 'beer' : 'juice';
-console.log(firstName + ' drinks ' + drink + '.');
+// var drink = age >= 18 ? 'beer' : 'juice';
+// console.log(firstName + ' drinks ' + drink + '.');
 
 
-// Switch statement
-var job = 'teacher';
-switch (job) {
-    case 'teacher':
-    case 'instructor':
-        console.log(firstName + ' teaches kids how to code.');
-        break;
-    case 'driver':
-        console.log(firstName + ' drives an uber in Lisbon.');
-        break;
-    case 'designer':
-        console.log(firstName + ' designs beautiful websites.');
-        break;
-    default:
-        console.log(firstName + ' does something else.');
+// // Switch statement
+// var job = 'teacher';
+// switch (job) {
+//     case 'teacher':
+//     case 'instructor':
+//         console.log(firstName + ' teaches kids how to code.');
+//         break;
+//     case 'driver':
+//         console.log(firstName + ' drives an uber in Lisbon.');
+//         break;
+//     case 'designer':
+//         console.log(firstName + ' designs beautiful websites.');
+//         break;
+//     default:
+//         console.log(firstName + ' does something else.');
+// }
+
+
+// // Converting our if/else statements to a switch statement.
+// var firstName = 'John';
+// var age = 16;
+
+// if (age < 13) {
+//     console.log(firstName + ' is a boy.')
+// } else if (age >= 13 && age < 20) { 
+//     console.log(firstName + ' is a teenager.')
+// } else if (age >= 20 && age < 30) {
+//     console.log(firstName + ' is a young man.')
+// } else {
+//     console.log(firstName + ' is a man.')
+// }
+
+// switch (true) {
+//     case age < 13:
+//         console.log(firstName + ' is a boy.');
+//         break;
+//     case age >= 13 && age < 20:
+//         console.log(firstName + ' is a teenager.');
+//         break;
+//     case age >= 20 && age < 30:
+//         console.log(firstName + ' is a young man.');
+//         break;
+//     default:
+//         console.log(firstName + ' is a man.');
+// }
+
+
+ /*********************
+ * Truthy and Falsy values and equality operators
+ */
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values.
+
+// var height;
+
+// height = 23;
+
+// if (height || height === 0 || height === '') {
+//     console.log('Variable is defined.');
+// } else {
+//     console.log('Variable has NOT been defined.');
+// }
+
+// // == Equality operator. Cnverts the string to a number, becasue it isn't strickt.
+// if (height == '23') {
+//     console.log('The == operator does type coercion!');
+// }
+
+
+
+/*********************
+ * CODING CHALLENGE 1
+ */
+
+var scoreJohn = (89 + 120 + 103) / 3;
+var scoreMike = (116 + 94 + 123) / 3;
+var scoreMary = (97 + 134 + 105) / 3;
+console.log (scoreJohn, scoreMike, scoreMary);
+
+// if (scoreJohn > scoreMike) {
+//     console.log('John\'s team wins with ' + scoreJohn + ' points');
+// } else if (scoreMike > scoreJohn) {
+//     console.log('Mike\'s team wins with ' + scoreMike + ' points');
+// } else {
+//     console.log('There is a draw');
+// }
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+    console.log('Mary\'s team wins with ' + scoreMary + ' points');
+} else {
+    console.log('There is a draw');
 }
+
+
